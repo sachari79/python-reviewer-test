2a0d1247615da1ca64258cd27b7b62cde6465793
sachari79 <swapan_achari@yahoo.com>
2021-06-06 19:17:01 -0400

    fix issues

--- subprocess-example.py
+++ subprocess-example.py
@@ -2,25 +2,26 @@ import subprocess
 import sys
 import boto3
 import datetime
+from shlex import split
  
 s3 = boto3.client('s3')
  
 def main(argv):
     cmd = argv
-    log_file_name = datetime.datetime.now().strftime("%m_%d_%Y") + "_logfile"
+    log_file_name = datetime.datetime.utcnow().strftime("%m_%d_%Y") + "_logfile"
     kickoff_subprocess(cmd, log_file_name)
     upload_output_to_S3(log_file_name)
  
 def kickoff_subprocess(cmd, log_file_name):
     process = subprocess.call(cmd, shell=True)
-    file = open(log_file_name, "a+")
-    timestamp = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
-    file.write(output)
+    with open(log_file_name, "a+") as file:
+        timestamp = datetime.datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")
+        output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
+        file.write(output)
  
 def upload_output_to_S3(log_file_name):
-    f = open(log_file_name, "rb")
-    s3.upload_fileobj(f, "swapan-data-01", log_file_name)
+    with open(log_file_name, "rb") as f:
+        s3.upload_fileobj(f, "swapan-data-01", log_file_name)
  
 if __name__ == "__main__":
    main(sys.argv[1:])
